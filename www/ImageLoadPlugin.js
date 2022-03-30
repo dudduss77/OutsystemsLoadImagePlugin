481b60e9f8fa0c6c7a3a97811365b1bf91fb1a1f
Damian Karbowiak <damiankarbowiak17@gmail.com>
2022-03-30 08:11:39 +0200

    Edit js and xml

--- www/ImageLoadPlugin.js
+++ www/ImageLoadPlugin.js
@@ -1,25 +1,25 @@
 var exec = require('cordova/exec');
 
-// exports.coolMethod = function (arg0, success, error) {
-//     exec(success, error, 'ImageLoadPlugin', 'coolMethod', [arg0]);
-// };
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'ImageLoadPlugin', 'coolMethod', [arg0]);
+};
 
 
-function ImageLoadPlugin() {}
+// function ImageLoadPlugin() {}
 
-ImageLoadPlugin.prototype.coolMethod = function(successCallback, errorCallback) {
-    var options = {};
+// ImageLoadPlugin.prototype.coolMethod = function(successCallback, errorCallback) {
+//     var options = {};
 
-    exec(successCallback, errorCallback, 'ImageLoadPlugin', 'coolMethod', [options]);
-}
+//     exec(successCallback, errorCallback, 'ImageLoadPlugin', 'coolMethod', [options]);
+// }
 
-ImageLoadPlugin.install = function() {
-    if(!window.plugins) {
-        window.plugins = {}
-    }
+// ImageLoadPlugin.install = function() {
+//     if(!window.plugins) {
+//         window.plugins = {}
+//     }
 
-    window.plugins.imageloadPlugin = new ImageLoadPlugin();
-    return window.plugins.imageloadPlugin;
-}
+//     window.plugins.imageloadPlugin = new ImageLoadPlugin();
+//     return window.plugins.imageloadPlugin;
+// }
 
-cordova.addConstructor(ImageLoadPlugin.install);
+// cordova.addConstructor(ImageLoadPlugin.install);
