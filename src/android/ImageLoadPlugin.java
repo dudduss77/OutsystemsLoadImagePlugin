af703bb01959f45e2062dd5206f8e2e6744a7f10
Damian Karbowiak <damiankarbowiak17@gmail.com>
2022-03-30 13:12:19 +0200

    Change thumbnails function

--- src/android/ImageLoadPlugin.java
+++ src/android/ImageLoadPlugin.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
+import android.util.Size;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -34,92 +35,84 @@ public class ImageLoadPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         Context context = cordova.getContext();
-        Log.i("Cur", "execute: ");
+        //Log.i("Cur", "execute: ");
         if (action.equals("coolMethod")) {
-            // String message = args.getString(0);
 
             String[] projection = new String[] {
-                    //MediaStore.Images.Media.DISPLAY_NAME,
                     MediaStore.Images.ImageColumns.DISPLAY_NAME,
                     MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
-                    MediaStore.Images.ImageColumns._ID
+                    MediaStore.Images.ImageColumns._ID,
+                    MediaStore.Images.ImageColumns.DATE_ADDED
             };
 
-            String selection = MediaStore.Images.Media.DATE_ADDED + ">= ?";
+            //String selection = MediaStore.Images.Media.DATE_ADDED + ">= ?";
 
             //String sortOrder = MediaStore.Images.Media.DISPLAY_NAME + " ASC";
-            String sortOrder = MediaStore.Images.ImageColumns.DISPLAY_NAME + " ASC";
+            String sortOrder = MediaStore.Images.ImageColumns.DATE_ADDED + " ASC";
 
             ContentResolver cr = context.getContentResolver();
 
             Cursor cur = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null, sortOrder);
 
-            Log.i("Cur", "onCreate: " + cur);
+            //Log.i("Cur", "onCreate: " + cur);
 
             JSONArray ret = new JSONArray();
+            try {
+                if(cur != null) {
+                    //Log.i("inside if", "insideIf");
+                    int idColumn = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID);
+                    int idColumnName = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME);
+                    int idBucketName = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME);
 
-            if(cur != null) {
-                Log.i("inside if", "insideIf");
-                int idColumn = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID);
-                int idColumnName = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME);
-                int idBucketName = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME);
+                    while(cur.moveToNext()) {
+                        JSONObject imagesData = new JSONObject();
+                        long id = cur.getLong(idColumn);
+                        String fileName = cur.getString(idColumnName);
+                        String bucketName = cur.getString(idBucketName);
 
-                //ArrayList<Bitmap> thumbnailsBitmap = new ArrayList<Bitmap>();
+                        Uri contentUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                        Bitmap thumbnail = null;
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                            try {
+                                thumbnail = context.getContentResolver().loadThumbnail(contentUri, new Size(100, 100), null);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            thumbnail = MediaStore.Images.Thumbnails.getThumbnail(context.getContentResolver(), id, MediaStore.Images.Thumbnails.MICRO_KIND, null);
+                        }
 
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        assert thumbnail != null;
+                        thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
 
-                while(cur.moveToNext()) {
-                    JSONObject imagesData = new JSONObject();
-                    long id = cur.getLong(idColumn);
-                    String fileName = cur.getString(idColumnName);
-                    String bucketName = cur.getString(idBucketName);
+                        byte[] byteArray = byteArrayOutputStream.toByteArray();
 
-                    // contentUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                        String encodedImage = Base64.encodeToString(byteArray, Base64.NO_WRAP);
 
-                    //Bitmap thumbnail = context.getContentResolver().loadThumbnail(contentUri, new Size(640, 480), null);
+                        imagesData.put("FileName", fileName);
+                        imagesData.put("BucketName", bucketName);
+                        imagesData.put("ImageData", encodedImage);
+                        ret.put(imagesData);
 
-                    Bitmap thumbnail = MediaStore.Images.Thumbnails.getThumbnail(context.getContentResolver(), id, MediaStore.Images.Thumbnails.MINI_KIND, null);
-
-                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                    thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-
-                    byte[] byteArray = byteArrayOutputStream.toByteArray();
-
-//                    try {
-//                        byteArrayOutputStream.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//
-//                    byte[] encodedImage = Base64.encode(byteArray, Base64.NO_WRAP);
-
-                    String encodedImage = Base64.encodeToString(byteArray, Base64.NO_WRAP);
-
-                    imagesData.put("FileName", fileName);
-                    imagesData.put("BucketName", bucketName);
-//                    imagesData.put("ImageData", new String(encodedImage));
-                    imagesData.put("ImageData", encodedImage);
-
-                    ret.put(imagesData);
-
-                    //imagesData = null;
-
-                    //thumbnailsBitmap.add(thumbnail);
-                    Log.i("Filename", "fileName: " + fileName);
-                    //Log.i("TAG", "execute: " + new String(encodedImage));
+                        //Log.i("Filename", "fileName: " + fileName);
+                        //Log.i("TAG", "execute: " + new String(encodedImage));
 //                    Log.i("Filename", "Json: " + imagesData);
-                    //Log.i("Filename", "fileUri: " + contentUri);
-                    //Log.i("Filename", "bucketName: " + bucketName);
-                    //Log.i("Filename", "encodedImage: " + encodedImage);
-                    //Log.i("Filename", "encodedImageLength: " + encodedImage.length());
-//                    Log.i("Filename", "Thumbnail Bitmap: " + thumbnail.getByteCount());
+                        //Log.i("Filename", "fileUri: " + contentUri);
+                        //Log.i("Filename", "bucketName: " + bucketName);
+                        //Log.i("Filename", "encodedImage: " + encodedImage);
+                        //Log.i("Filename", "encodedImageLength: " + encodedImage.length());
+                        //Log.i("Filename", "Thumbnail Bitmap: " + thumbnail.getByteCount());
 //                    Log.i("Filename", "Thumbnail Width: " + thumbnail.getWidth());
 //                    Log.i("Filename", "Thumbnail Height: " + thumbnail.getHeight());
+                    }
                 }
+            } finally {
+                assert cur != null;
+                cur.close();
             }
 
-
-            String message = "Hallo i working";
-            Log.i("TT", "ret" +  ret.toString());
+            //Log.i("TT", "ret" +  ret.toString());
             this.coolMethod(ret.toString(), callbackContext);
             return true;
         }
