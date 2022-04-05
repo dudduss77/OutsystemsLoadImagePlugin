331eecc77c48d1bb1a28ab37c2f5f2f6c0e2bf3a
Damian Karbowiak <damiankarbowiak17@gmail.com>
2022-04-05 08:48:45 +0200

    Add Limit and Offset

--- src/android/ImageLoadPlugin.java
+++ src/android/ImageLoadPlugin.java
@@ -8,6 +8,7 @@ import android.graphics.Bitmap;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
@@ -47,23 +48,23 @@ public class ImageLoadPlugin extends CordovaPlugin {
         Context context = cordova.getContext();
         Log.i("Cur", "action: " + action);
         if (action.equals("coolMethod")) {
+            Log.i("ttt", "json " + args);
             String bucketName = args.getJSONObject(0).getString("BucketName");
+            int limit = args.getJSONObject(0).getInt("Limit");
+            int offset = args.getJSONObject(0).getInt("Offset");
             Log.i("TAG", "args: " + bucketName);
+            Log.i("TAG", "args: " + limit);
+            Log.i("TAG", "args: " + offset);
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        coolMethod(getImageFromStorage(context, bucketName).toString(), callbackContext);
+                        coolMethod(getImageFromStorage(context, bucketName, limit, offset).toString(), callbackContext);
                     } catch (JSONException | ExecutionException | InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             });
-//            try {
-//                this.coolMethod(getImageFromStorage(context, bucketName).toString(), callbackContext);
-//            } catch (ExecutionException | InterruptedException e) {
-//                e.printStackTrace();
-//            }
             return true;
         } else if (action.equals("getBucketName")) {
             cordova.getThreadPool().execute(new Runnable() {
@@ -117,8 +118,7 @@ public class ImageLoadPlugin extends CordovaPlugin {
         return jsonArray.toString();
     }
 
-    private JSONArray getImageFromStorage(Context context, String inBucketName) throws JSONException, ExecutionException, InterruptedException {
-
+    private JSONArray getImageFromStorage(Context context, String inBucketName, int inLimit, int inOffset) throws JSONException, ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(10);
         List<Future<JSONObject>> future = new ArrayList<>();
         String[] projection = new String[] {
@@ -127,60 +127,43 @@ public class ImageLoadPlugin extends CordovaPlugin {
                 MediaStore.Images.ImageColumns._ID,
                 MediaStore.Images.ImageColumns.DATE_ADDED,
                 MediaStore.Images.ImageColumns.DATA
-
         };
 
         String selection = MediaStore.Images.Media.BUCKET_DISPLAY_NAME + "=?";
         String[] selectionArgs = {inBucketName};
-        String sortOrder = MediaStore.Images.ImageColumns.DATE_ADDED + " DESC";
+        String sortOrder = MediaStore.Images.ImageColumns.DATE_ADDED + " DESC LIMIT " + inLimit + " OFFSET " + inOffset;
 
         ContentResolver cr = context.getContentResolver();
 
-        Cursor cur = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, selection, selectionArgs, sortOrder);
-
-        //Log.i("Cur", "onCreate: " + cur);
+        Cursor cur = null;
+        if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+            Bundle queryArgs = new Bundle();
+            String[] sortArgs = {MediaStore.Images.ImageColumns.DATE_ADDED};
+            queryArgs.putStringArray(ContentResolver.QUERY_ARG_SORT_COLUMNS, sortArgs);
+            queryArgs.putInt(ContentResolver.QUERY_ARG_SORT_DIRECTION, ContentResolver.QUERY_SORT_DIRECTION_DESCENDING);
+            queryArgs.putInt(ContentResolver.QUERY_ARG_LIMIT, inLimit);
+            queryArgs.putInt(ContentResolver.QUERY_ARG_OFFSET, inOffset);
+            queryArgs.putString(ContentResolver.QUERY_ARG_SQL_SELECTION, selection);
+            queryArgs.putStringArray(ContentResolver.QUERY_ARG_SQL_SELECTION_ARGS, selectionArgs);
+            cur = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, queryArgs, null);
+        } else {
+            cur = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, selection, selectionArgs, sortOrder);
+        }
 
         JSONArray ret = new JSONArray();
         try {
             if(cur != null) {
-                //Log.i("inside if", "insideIf");
                 int idColumn = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID);
                 int idColumnName = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME);
                 int idBucketName = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME);
                 int idPath = cur.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA);
                 while(cur.moveToNext()) {
-//                    JSONObject imagesData = new JSONObject();
                     long id = cur.getLong(idColumn);
                     String fileName = cur.getString(idColumnName);
                     String bucketName = cur.getString(idBucketName);
                     String path = cur.getString(idPath);
 
                     Uri contentUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-//                    Bitmap thumbnail = null;
-//                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-//                        try {
-//                            thumbnail = context.getContentResolver().loadThumbnail(contentUri, new Size(500, 500), null);
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    } else {
-//                        thumbnail = MediaStore.Images.Thumbnails.getThumbnail(context.getContentResolver(), id, MediaStore.Images.Thumbnails.MINI_KIND, null);
-//                    }
-//
-//                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//                    assert thumbnail != null;
-//                    thumbnail.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
-//
-//                    byte[] byteArray = byteArrayOutputStream.toByteArray();
-//
-//                    String encodedImage = Base64.encodeToString(byteArray, Base64.NO_WRAP);
-//
-//                    imagesData.put("FileName", fileName);
-//                    imagesData.put("BucketName", bucketName);
-//                    imagesData.put("Path", path);
-//                    imagesData.put("ImageData", encodedImage);
-//                    ret.put(imagesData);
-
 
                     future.add(executor.submit(new Callable<JSONObject>() {
                         @Override
@@ -228,17 +211,7 @@ public class ImageLoadPlugin extends CordovaPlugin {
                             return imagesData;
                         }
                     }));
-
-                    //Log.i("Filename", "fileName: " + fileName);
-                    //Log.i("TAG", "execute: " + new String(encodedImage));
-//                    Log.i("Filename", "Json: " + imagesData);
-//                    Log.i("Filename", "fileUri: " + contentUri);
-                    //Log.i("Filename", "bucketName: " + bucketName);
-                    //Log.i("Filename", "encodedImage: " + encodedImage);
-                    //Log.i("Filename", "encodedImageLength: " + encodedImage.length());
-                    //Log.i("Filename", "Thumbnail Bitmap: " + thumbnail.getByteCount());
-//                    Log.i("Filename", "Thumbnail Width: " + thumbnail.getWidth());
-//                    Log.i("Filename", "Thumbnail Height: " + thumbnail.getHeight());
+                    Log.i("TAG", "getImageFromStorage: " + cur.getPosition());
                 }
 
             }
